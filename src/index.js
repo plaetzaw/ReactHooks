d1425c23db751d9615157e238db87f1948b34be9
plaetzaw <plaetzaw@gmail.com>
2021-01-04 17:00:58 -0600

    Setting up Use Memo

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import useEffect from './useEffect'
 import useEffect2 from './LearnUE2'
 import useRef from './LearnUseRef'
+import useMemo from './LearnUseMemo'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 ReactDOM.render(
@@ -14,6 +15,7 @@ ReactDOM.render(
     <Route exact path="/ue" component={useEffect}/>
     <Route exact path="/ue2" component={useEffect2}/>
     <Route exact path ="/ur" component={useRef}/>
+    <Route exact path ="/um" component={useMemo}/>
 
   </Switch>
   </BrowserRouter>,
