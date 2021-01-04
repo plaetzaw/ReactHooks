5dcb04a2ffdd2a43cfde258fa51f4780140ddc52
plaetzaw <plaetzaw@gmail.com>
2021-01-04 16:05:06 -0600

    UE example 1

--- src/useEffect.js
+++ src/useEffect.js
@@ -2,6 +2,15 @@ import React, {useState, useEffect} from 'react'
 
 function LearnUE() {
     const [resourceType, setResourceType] = useState('posts')
+    const [items, setItems] = useState([])
+
+    useEffect (() => {
+  fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  .then(response => response.json())
+  .then(json => setItems(json))
+    }, [resourceType])
+    //The second value of the useEffect hook is an array, whenever the values of that array are updated, the component will re-render
+    //So if you pass in an empty array, it will ONLY render on the inital mount
 
     return (
         <div>
@@ -9,6 +18,9 @@ function LearnUE() {
            <button onClick={() => setResourceType('users')}>Users</button>
            <button onClick={() => setResourceType('comments')}>Comments</button>
             <h1>{resourceType}</h1>
+            {items.map(item => {
+                return <pre>{JSON.stringify(item)}</pre>
+            })}
         </div>
     )
 }
