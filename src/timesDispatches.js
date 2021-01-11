b2d77efbfc64664c7f9fa5446229036a6dd356c9
plaetzaw <plaetzaw@gmail.com>
2021-01-11 14:13:42 -0600

    Reducer logic

--- src/timesDispatches.js
+++ src/timesDispatches.js
@@ -19,3 +19,45 @@ export const initialState = {
         text: "200 Free",
     }]
 }
+
+export const reducer = (state, action) => {
+    switch (action.type){
+        case ACTIONS.ADD:
+            {
+                const newEntry = state.entries + 1;
+                const newTime = {
+                    id: newEntry,
+                    text: action.text
+                };
+                return {
+                    entries: newEntry,
+                    times: [...state, newTime]
+                }
+            }
+        case ACTIONS.EDIT:
+            {
+                const idx = state.times.findIndex(t => t.id === action.id);
+                const time = Object.assign({}, state.times[idx]);
+                time.text = action.text;
+                const times = Object.assign([], state.times);
+                times.splice(idx, 1, time);
+                return {
+                    entries: state.entries,
+                    times: times,
+                }
+            }
+        case ACTIONS.DELETE: 
+        {
+                const idx = state.times.findIndex(t => t.id === action.id);
+                const times = Object.assign([], state.times);
+                times.splice(idx, 1);
+                return {
+                    entries: state.entries,
+                    times: times,
+                }
+        }  
+        default:
+        return state;
+
+    }
+}
